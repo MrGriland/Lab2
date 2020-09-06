f6bc9486ef7c90847c0c07387bf44d6c4c69df74
Griland <grishabulgak@gmail.com>
2020-09-06 21:12:24 +0300

    1a

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,7 +5,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool q1 = true;
+            byte q2 = 3;
+            sbyte q3 = -3;
+            char q4 = 'f';
+            decimal q5 = 300;
+            double q6 = 200.4;
+            float q7 = 3.6f;
+            int q8 = 255;
+            uint q9 = 256;
+            long q10 = 555;
+            ulong q11 = 556;
+            short q12 = 222;
+            ushort q13 = 223;
+            string q14 = "afsafa";
+            Console.WriteLine("{0}\n{1}\n{2}\n{3}\n{4}\n{5}\n{6}\n{7}\n{8}\n{9}\n{10}\n{11}\n{12}\n{13}", q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14);
         }
     }
 }
