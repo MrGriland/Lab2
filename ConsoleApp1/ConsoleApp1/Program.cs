dd9270017656f748b8d767a9829f3f8c60cb2bb4
Griland <grishabulgak@gmail.com>
2020-09-11 01:36:06 +0300

    1b

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp1
             int x = 9;
             while (x!=0)
             Console.WriteLine("Input number of task");
-            Convert.ToInt16(Console.ReadLine());
+            x = Convert.ToInt16(Console.ReadLine());
             switch(x)
             {
                 case 1: Taskone();
@@ -38,7 +38,21 @@ namespace ConsoleApp1
             ushort q13 = 223;
             string q14 = "afsafa";
             Console.WriteLine("{0}\n{1}\n{2}\n{3}\n{4}\n{5}\n{6}\n{7}\n{8}\n{9}\n{10}\n{11}\n{12}\n{13}", q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14);
-
+            int num1 = 2147;
+            long bigNum1 = num1;
+            float num2 = 134.45E-2f;
+            double bigNum2 = num2;
+            byte num3 = 5;
+            short bigNum3 = num3;
+            long num4 = 87;
+            decimal bigNum4 = num4;
+            ushort num5 = 56;
+            int bigNum5 = num5;
+            int smallNum1 = (int)bigNum2;
+            long smallNum2 = (long)bigNum4;
+            decimal smallNum3 = (decimal)num2;
+            short smallNum4 = (short)bigNum5;
+            uint smallNum5 = (uint)bigNum1;
         }
         static void Tasktwo()
         {
