299d8856bb895554e64a63b5144e005e6a4d61de
Griland <grishabulgak@gmail.com>
2020-09-12 00:04:26 +0300

    1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,19 +6,24 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int x = 9;
-            while (x!=0)
-            Console.WriteLine("Input number of task");
-            x = Convert.ToInt16(Console.ReadLine());
-            switch(x)
+            while (x != 0)
             {
-                case 1: Taskone();
-                    break;
-                case 2: Tasktwo();
-                    break;
-                case 3: Taskthree();
-                    break;
-                default:
-                    return;
+                Console.WriteLine("Input number of task");
+                x = Convert.ToInt16(Console.ReadLine());
+                switch (x)
+                {
+                    case 1:
+                        Taskone();
+                        break;
+                    case 2:
+                        Tasktwo();
+                        break;
+                    case 3:
+                        Taskthree();
+                        break;
+                    default:
+                        return;
+                }
             }
         }
         static void Taskone()
@@ -40,7 +45,7 @@ namespace ConsoleApp1
             Console.WriteLine("{0}\n{1}\n{2}\n{3}\n{4}\n{5}\n{6}\n{7}\n{8}\n{9}\n{10}\n{11}\n{12}\n{13}", q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14);
             int num1 = 2147;
             long bigNum1 = num1;
-            float num2 = 134.45E-2f;
+            float num2 = 134.45f;
             double bigNum2 = num2;
             byte num3 = 5;
             short bigNum3 = num3;
@@ -53,6 +58,13 @@ namespace ConsoleApp1
             decimal smallNum3 = (decimal)num2;
             short smallNum4 = (short)bigNum5;
             uint smallNum5 = (uint)bigNum1;
+            int price = 39;
+            Object obj = price;
+            int price2 = (int)obj;
+            var f = 0.34F;
+            double? d1 = 3.3;
+            var j = 3;
+            //j = 0.34F;
         }
         static void Tasktwo()
         {
@@ -62,5 +74,6 @@ namespace ConsoleApp1
         {
 
         }
+        
     }
 }
